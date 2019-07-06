8cdbf56dd82f77c3f01fef223b1ec96420ed06fe
Axel-Jacobsen <axelnj44@gmail.com>
2019-07-06 16:54:29 -0700

    small structural code changes

--- cylinders.js
+++ cylinders.js
@@ -1,49 +1,57 @@
+'use strict'
+
+class Geometry {
+    constructor(A, B) {
+        self.A = A;
+        self.B = B;
+    }
+    position_on_rotation(theta, xs, ys) {
+        if (xs.length !== ys.length) {
+            console.log('length of xs and ys must be equal');
+            return false;
+        }
+        const xs_new = [];
+        const ys_new = [];
+        for (let i = 0; i < xs.length; i++) {
+            let x = xs[i];
+            let y = ys[i];
+            let r = Math.sqrt(x * x + y * y);
+            let f = theta * A * A / (B * B - A * A) * (B * B / r - r);
+            xs_new.push(-Math.sin(theta) * f + x);
+            ys_new.push(Math.cos(theta) * f + y);
+        }
+        return [xs_new, ys_new];
+    }
+    circ(N, x0 = 0, y0 = 0, scale = 1) {
+        let xs = [];
+        let ys = [];
+        for (let i = 0; i < N; i++) {
+            xs.push(Math.cos(2 * Math.PI * i / N) * scale + x0);
+            ys.push(Math.sin(2 * Math.PI * i / N) * scale + y0);
+        }
+        return [xs, ys];
+    }
+}
+
+
 const A = 2
 const B = 10
 let THETA = 0
 
-const position_on_rotation = (theta, xs, ys) => {
-    if (xs.length !== ys.length) {
-        console.log('length of xs and ys must be equal');
-        return false
-    }
-
-    const xs_new = []
-    const ys_new = []
-
-    for (let i = 0; i < xs.length; i++) {
-        let x = xs[i]
-        let y = ys[i]
-        let r = Math.sqrt(x * x + y * y)
-        let f = theta * A * A / (B * B - A * A) * (B * B / r - r)
-
-        xs_new.push(-Math.sin(theta) * f + x)
-        ys_new.push(Math.cos(theta) * f + y)
-    }
-
-    return [xs_new, ys_new]
-}
-
-const circ = (N, x0 = 0, y0 = 0, scale = 1) => {
-    let xs = []
-    let ys = []
-
-    for (let i = 0; i < N; i++) {
-        xs.push(Math.cos(2 * Math.PI * i / N) * scale + x0)
-        ys.push(Math.sin(2 * Math.PI * i / N) * scale + y0)
-    }
-
-    return [xs, ys]
-}
-
 const get_data = () => {
-    let circle = circ(1000, 3, 3)
-    let rotated_circle = position_on_rotation(THETA, circle[0], circle[1])
+    const geo = new Geometry(A, B)
+    let circle = geo.circ(1000, 3, 3)
+    let rotated_circle = geo.position_on_rotation(THETA, circle[0], circle[1])
 
-    const r_rotated = {
-        x: rotated_circle[0],
-        y: rotated_circle[1],
-        type: 'markers'
+
+    const inner_circle = {
+        x: geo.circ(100, 0, 0, A)[0],
+        y: geo.circ(100, 0, 0, A)[1]
+    }
+
+    const outer_circle = {
+        x: geo.circ(100, 0, 0, B)[0],
+        y: geo.circ(100, 0, 0, B)[1]
     }
 
     const r0 = {
@@ -51,20 +59,16 @@ const get_data = () => {
         y: circle[1]
     }
 
-    const inner_circle = {
-        x: circ(100, 0, 0, A)[0],
-        y: circ(100, 0, 0, A)[1]
+    const r_rotated = {
+        x: rotated_circle[0],
+        y: rotated_circle[1],
+        type: 'markers'
     }
 
-    const outer_circle = {
-        x: circ(100, 0, 0, B)[0],
-        y: circ(100, 0, 0, B)[1]
-    }
-
-    return [r0, inner_circle, outer_circle, r_rotated]
+    return [inner_circle, outer_circle, r0, r_rotated]
 }
 
-let DATA = get_data()
+const DATA = get_data()
 
 const layout = {
     autosize: true,
@@ -81,16 +85,17 @@ const layout = {
     }
 }
 
-document.onkeypress = function (e) {
+document.onkeypress = e => {
 
-    e = e || window.event;
+    e = e || window.event
 
     // u = 117, d = 100
     if (e.keyCode === 117) {
         THETA = THETA + 0.1
-    }
-    if (e.keyCode === 100) {
+    } else if (e.keyCode === 100) {
         THETA = THETA - 0.1
+    } else {
+        return
     }
 
     let div = document.getElementById('theta');
@@ -100,9 +105,8 @@ document.onkeypress = function (e) {
         x: get_data()[3].x,
         y: get_data()[3].y
     }
-    console.log(DATA);
-    
+
     Plotly.redraw('chart')
-};
+}
 
 Plotly.newPlot('chart', DATA, layout)
