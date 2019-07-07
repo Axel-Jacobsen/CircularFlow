5a2248b421c7d8e94e9b396c2ae15445b975e83e
Axel-Jacobsen <axelnj44@gmail.com>
2019-07-07 15:34:01 -0700

    restructuring/renaming of some methods

--- cylinders.js
+++ cylinders.js
@@ -1,11 +1,13 @@
 'use strict'
 
 class Geometry {
+
     constructor(A, B) {
         self.A = A;
         self.B = B;
     }
-    position_on_rotation(theta, xs, ys) {
+
+    rotate_inner_ring(theta, xs, ys) {
         if (xs.length !== ys.length) {
             console.log('length of xs and ys must be equal');
             return false;
@@ -16,12 +18,31 @@ class Geometry {
             let x = xs[i];
             let y = ys[i];
             let r = Math.sqrt(x * x + y * y);
-            let f = theta * A * A / (B * B - A * A) * (B * B / r - r);
-            xs_new.push(-Math.sin(theta) * f + x);
-            ys_new.push(Math.cos(theta) * f + y);
+            let f = theta * self.A * self.A / (self.B * self.B - self.A * self.A) * (self.B * self.B / r - r);
+            xs_new.push(Math.cos(theta) * f + x);
+            ys_new.push(-Math.sin(theta) * f + y);
         }
         return [xs_new, ys_new];
     }
+
+    rotate_around_origin(theta, xs, ys) {
+        if (xs.length !== ys.length) {
+            console.log('length of xs and ys must be equal');
+            return false;
+        }
+        const xs_new = [];
+        const ys_new = [];
+        for (let i = 0; i < xs.length; i++) {
+            let x = xs[i];
+            let y = ys[i];
+            let r = Math.sqrt(x * x + y * y);
+            let f = theta * self.A * self.A / (self.B * self.B - self.A * self.A) * (self.B * self.B / r - r);
+            xs_new.push(Math.cos(theta) * f + x);
+            ys_new.push(-Math.sin(theta) * f + y);
+        }
+        return [xs_new, ys_new];
+    }
+
     circ(N, x0 = 0, y0 = 0, scale = 1) {
         let xs = [];
         let ys = [];
@@ -29,20 +50,22 @@ class Geometry {
             xs.push(Math.cos(2 * Math.PI * i / N) * scale + x0);
             ys.push(Math.sin(2 * Math.PI * i / N) * scale + y0);
         }
+        // Complete the circle
+        xs.push(xs[0])
+        ys.push(ys[0])
         return [xs, ys];
     }
 }
 
 
-const A = 2
-const B = 10
-let THETA = 0
+const A = 2  // Inner radius
+const B = 10 // Outer radius
+let THETA = 0 // Angle of rotation of inner radius
 
 const get_data = () => {
     const geo = new Geometry(A, B)
     let circle = geo.circ(1000, 3, 3)
-    let rotated_circle = geo.position_on_rotation(THETA, circle[0], circle[1])
-
+    let rotated_circle = geo.rotate_inner_ring(THETA, circle[0], circle[1])
 
     const inner_circle = {
         x: geo.circ(100, 0, 0, A)[0],
@@ -74,7 +97,9 @@ const layout = {
     autosize: true,
     width: 600,
     height: 600,
-    show_legend: false,
+    showlegend: false,
+    paper_bgcolor: '#f0f0f0',
+    plot_bgcolor: '#f0f0f0',
     xaxis: {
         constrain: 'domain',
         range: [-11, 11]
@@ -89,17 +114,18 @@ document.onkeypress = e => {
 
     e = e || window.event
 
-    // u = 117, d = 100
-    if (e.keyCode === 117) {
+    if (e.key === 'j') {
         THETA = THETA + 0.1
-    } else if (e.keyCode === 100) {
+    } else if (e.key === 'k') {
         THETA = THETA - 0.1
+    } else if (e.key === 'l') {
+        THETA = 0
     } else {
         return
     }
 
     let div = document.getElementById('theta');
-    div.innerHTML = THETA;
+    div.innerHTML = '&Theta; = ' + THETA.toFixed(1) + ' radians'
 
     DATA[3] = {
         x: get_data()[3].x,
@@ -109,4 +135,4 @@ document.onkeypress = e => {
     Plotly.redraw('chart')
 }
 
-Plotly.newPlot('chart', DATA, layout)
+Plotly.newPlot('chart', DATA, layout, { modeBarButtonsToRemove: ['toImage'] })
