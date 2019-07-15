de2b1b5767f6729b8403a17aa0846b464f353541
Axel-Jacobsen <axelnj44@gmail.com>
2019-07-14 21:22:30 -0700

    restructure function return types to return dictionaries instead of arrays of arrays

--- cylinders.js
+++ cylinders.js
@@ -20,19 +20,23 @@ class Geometry {
             let r = Math.sqrt(x * x + y * y);
             let f = theta * this.A * this.A / (this.B * this.B - this.A * this.A) * (this.B * this.B / r - r);
             let x_y_new = this.rotate(f, x, y)
-            xs_new.push(x_y_new[0])
-            ys_new.push(x_y_new[1])
+            xs_new.push(x_y_new.xs)
+            ys_new.push(x_y_new.ys)
         }
-        return [xs_new, ys_new];
+        return {
+            xs: xs_new,
+            ys: ys_new
+        }
     }
 
     rotate(theta, x, y) {
         // v_new = R*v, v = (x,y)^T, 
         // R = [[cos(theta) -sin(theta)], 
         //      [sin(theta) cos(theta)]]
-        let x_new = x * Math.cos(theta) - y * Math.sin(theta);
-        let y_new = x * Math.sin(theta) + y * Math.cos(theta);
-        return [x_new, y_new]
+        return {
+            x: x * Math.cos(theta) - y * Math.sin(theta),
+            y: x * Math.sin(theta) + y * Math.cos(theta)
+        }
     }
 
     rotate_points(theta, xs, ys) {
@@ -40,14 +44,17 @@ class Geometry {
             console.log('length of xs and ys must be equal');
             return false;
         }
-        const xs_new = [];
-        const ys_new = [];
+        const xs_new = []
+        const ys_new = []
         for (let i = 0; i < xs.length; i++) {
             let x_y_new = this.rotate(theta, xs[i], ys[i])
-            xs_new.push(x_y_new[0]);
-            ys_new.push(x_y_new[1]);
+            xs_new.push(x_y_new.x)
+            ys_new.push(x_y_new.y)
         }
-        return [xs_new, ys_new];
+        return {
+            xs: xs_new,
+            ys: ys_new
+        }
     }
 
     circ(N = 100, x0 = 0, y0 = 0, scale = 1) {
@@ -57,10 +64,12 @@ class Geometry {
             xs.push(Math.cos(2 * Math.PI * i / N) * scale + x0);
             ys.push(Math.sin(2 * Math.PI * i / N) * scale + y0);
         }
-        // Complete the circle
-        xs.push(xs[0])
-        ys.push(ys[0])
-        return [xs, ys];
+        xs.push(xs[0])  // Complete the circle
+        ys.push(ys[1])
+        return {
+            xs: xs,
+            ys: ys
+        }
     }
 
     line(N = 100, x0, y0, x1, y1) {
@@ -70,25 +79,28 @@ class Geometry {
             xs.push(x0 + (x1 - x0) * i / N)
             ys.push(y0 + (y1 - y0) * i / N)
         }
-        return [xs, ys]
+        return {
+            xs: xs_new,
+            ys: ys_new
+        }
     }
 }
 
 class Transform {
 
     static joukowsky(xs, ys, lambda) {
-        const ws = []
         const xs_new = []
         const ys_new = []
         for (let i = 0; i < xs.length; i++) {
             let z = math.complex(xs[i], ys[i])
             let w = math.add(z, math.divide(lambda * lambda, z))
-            console.log(i, z, w);
-
             xs_new.push(math.re(w))
             ys_new.push(math.im(w))
         }
-        return [xs_new, ys_new]
+        return {
+            xs: xs_new,
+            ys: ys_new
+        }
     }
 
 }
@@ -100,10 +112,10 @@ let THETA = 0 // Angle of rotation of inner radius
 const get_rotated_ring = () => {
     const geo = new Geometry(A, B)
     let shape = geo.circ(1000, 3, 3)
-    let rotated_circle = geo.rotate_inner_ring(THETA, shape[0], shape[1])
+    let rotated_circle = geo.rotate_inner_ring(THETA, shape.xs, shape.ys)
     return {
-        x: rotated_circle[0],
-        y: rotated_circle[1],
+        x: rotated_circle.xs,
+        y: rotated_circle.ys,
         type: 'markers'
     }
 }
@@ -114,15 +126,15 @@ const get_airfoil_data = () => {
     const x_off = 0.1
     const y_off = 0
     const circle = geo.circ(1000, x_off, y_off, r)
-    const airfoil = Transform.joukowsky(circle[0], circle[1], r - math.sqrt(x_off * x_off + y_off * y_off))
+    const airfoil = Transform.joukowsky(circle.xs, circle.ys, r - math.sqrt(x_off * x_off + y_off * y_off))
     return [{
-        x: circle[0],
-        y: circle[1],
+        x: circle.xs,
+        y: circle.ys,
         type: 'markers'
     },
     {
-        x: airfoil[0],
-        y: airfoil[1],
+        x: airfoil.xs,
+        y: airfoil.ys,
         type: 'markers'
     }]
 }
@@ -133,18 +145,18 @@ const get_tc_data = () => {
     let shape = geo.circ(100, 3, 3)
 
     const inner_circle = {
-        x: geo.circ(100, 0, 0, A)[0],
-        y: geo.circ(100, 0, 0, A)[1]
+        x: geo.circ(100, 0, 0, A).xs,
+        y: geo.circ(100, 0, 0, A).ys
     }
 
     const outer_circle = {
-        x: geo.circ(100, 0, 0, B)[0],
-        y: geo.circ(100, 0, 0, B)[1]
+        x: geo.circ(100, 0, 0, B).xs,
+        y: geo.circ(100, 0, 0, B).ys
     }
 
     const r0 = {
-        x: shape[0],
-        y: shape[1]
+        x: shape.xs,
+        y: shape.ys
     }
 
     const r_rotated = get_rotated_ring()
